b7a7c91f9e8ce5d413812cc05750ee0718c20c66
giangm9 <giangm9@gmail.com>
2016-03-10 15:20:35 +0700

    update lab3

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -9,8 +9,8 @@
 #include <math.h>
 using namespace std;
 
-#define kWIN_WIDTH 640.0f
-#define kWIN_HEIGHT 640.0f
+#define kWIN_WIDTH 1000.0f
+#define kWIN_HEIGHT 1000.0f
 #define PI 3.1416
 
 
@@ -22,7 +22,6 @@ void simpleInit(){
     glLoadIdentity( );
     glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
-
 
 void setWindow(float left, float right, float bottom, float top)
 {
@@ -36,7 +35,6 @@ float kochX[] = {0.0f, .33333f, .5000f, .66666f, 1.0f };
 float kochY[] = {0.5f, .50000f, .7886f, .50000f, 0.5f };
 
 int kochLen = 5;
-
 
 void drawFlake(){
     glBegin(GL_LINE_STRIP);
@@ -53,7 +51,7 @@ void displayAtGen(int n ){
 
     }
     else {
-        drawFlake();
+        drawFlake();				
         glScalef(.33333, .33333, .3333f);
 
         glRotatef(60, 0, 0, 1.0);
@@ -91,11 +89,11 @@ void flakeDisplay(void){
 
     float scaleRate = sqrt(3) / 6;
 
-    glScalef(.7, .7 ,.7);
+    glScalef(.8, .8 ,1);
     for (int i = 0; i < 3; i++){
 
         glTranslatef(0, scaleRate, 0);
-        displayAtGen(5);
+				displayAtGen(5);
         glTranslatef(0, -scaleRate, 0);
         glRotatef(120.0f, 0, 0, 1.0f);
     }
@@ -103,17 +101,86 @@ void flakeDisplay(void){
     glutSwapBuffers();
 }
 
+void functionsDisplay(){
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	glViewport(0, 0, kWIN_WIDTH/2, kWIN_HEIGHT/2);
+
+
+	//cardidoid
+	setWindow(-2.0, 2.0, -2.0, 2.0);
+	glColor3f(1.0, 1.0, 1.0);
+	int scaleK = 1;
+	int len = 200;
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	glBegin(GL_LINE_STRIP);
+	for (int i = 0; i <= len; i++){
+		float t = (2.0f*PI / len) * i;
+		float x = scaleK * (1 + cos(t)) * cos(t) - .5;
+		float y = scaleK * (1 + cos(t)) * sin(t);
+		glVertex3f(x, y, .0);
+	}
+	glEnd();
+
+	// rose 3
+	glViewport(kWIN_WIDTH/2, 0, kWIN_WIDTH/2, kWIN_HEIGHT/2);
+	int n = 8;
+	len = n*200;
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_LINE_STRIP);
+	for (int i = 0; i < len; i++){
+		float t = (2.0f*n*PI / len) * i;
+		float x = scaleK * (cos(t*n)) * cos(t);
+		float y = scaleK * (cos(t*n)) * sin(t);
+		glVertex3f(x, y, .0);
+	}
+	glEnd();
+
+
+
+	// spiral
+	glViewport(kWIN_WIDTH/2, kWIN_HEIGHT/2, kWIN_WIDTH/2, kWIN_HEIGHT/2);
+
+	scaleK = 1.0;
+	len = 200;
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glBegin(GL_LINE_STRIP);
+	for (int i = 0; i < len; i++){
+		float t = (10.0 / len) * i;
+		float x = scaleK * exp(2*t) * cos(t);
+		float y = scaleK * exp(2*t) * sin(t);
+		glVertex3f(x, y, .0);
+	}
+	glEnd();
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+
+
+
 int main( int argc, char *argv[] )  {
     glutInit( &argc, argv );
     glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
     glutInitWindowSize( kWIN_WIDTH, kWIN_WIDTH );
 
-    glutInitWindowPosition( 700, 0 );
+		// flake
+		glutInitWindowPosition( 120, 0 );
     glutCreateWindow( "lab3_flake");
     glutReshapeWindow(kWIN_WIDTH, kWIN_HEIGHT);
 
     simpleInit();
     glutDisplayFunc(flakeDisplay);
+
+		// functions
+		glutInitWindowPosition( 100, 0 );
+		glutCreateWindow( "lab3_cardidoid");
+		glutReshapeWindow(kWIN_WIDTH, kWIN_HEIGHT);
+
+		simpleInit();
+		glutDisplayFunc(functionsDisplay);
 
     glutMainLoop( );
 
