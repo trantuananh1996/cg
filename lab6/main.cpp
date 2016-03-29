1aa5ad2b7f7b3cc47fbd1dd1c2d9a432b8b0046e
giangm9 <giangm9@gmail.com>
2016-03-29 14:48:44 +0700

    update lab6

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -4,9 +4,39 @@
 #include "BodyPart.h"
 #include <cstdio>
 
-BodyPart lhand, rhand;
-BodyPart body;
-BodyPart lfoot, rfoot;
+BodyPart *lhand, *rhand;
+BodyPart *body;
+BodyPart *lfoot, *rfoot;
+
+void initData(){
+    Vec2 bodyLines[] = {Vec2(.5, .3), Vec2(.5, .6)};
+    float bodyTime[] = {1.0f};
+    body =  new BodyPart(1, bodyLines, bodyTime);
+
+    //hands
+    Vec2 lhLines[] = {Vec2(.5, .6), Vec2(.4, .4),
+                     Vec2(.5, .6), Vec2(.6, .4)};
+    float lhTime[] = {.0, 1.0f};
+    lhand =  new BodyPart(2, lhLines, lhTime);
+
+    Vec2 rhLines[] = {Vec2(.5, .6), Vec2(.6, .4),
+                     Vec2(.5, .6), Vec2(.4, .4)};
+    float rhTime[] = {.0, 1.0f};
+    rhand =  new BodyPart(2, rhLines, rhTime);
+
+
+    //foots
+    Vec2 lfLines[] = {Vec2(.5, .3), Vec2(.55, .1),
+                     Vec2(.5, .3), Vec2(.45, .1)};
+    float lfTime[] = {.0, 1.0f};
+    lfoot =  new BodyPart(2, lfLines, lfTime);
+
+    Vec2 rfLines[] = {Vec2(.5, .3), Vec2(.45, .1),
+                     Vec2(.5, .3), Vec2(.55, .1)};
+    float rfTime[] = {.0, 1.0f};
+    rfoot =  new BodyPart(2,rfLines, rfTime);
+
+}
 
 
 
@@ -17,6 +47,7 @@ void myInit( void )  {
   glMatrixMode( GL_PROJECTION );
   glLoadIdentity( );
   glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+  initData();
 }
 
 void tickBodyPart(BodyPart *part);
@@ -25,7 +56,11 @@ void myDisplay( void )  {
   glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
   glColor3f(1.0f, 1.0f, 1.0f);
 
-  tickBodyPart(&hand);
+  tickBodyPart(body);
+  tickBodyPart(lhand);
+  tickBodyPart(rhand);
+  tickBodyPart(lfoot);
+  tickBodyPart(rfoot);
 
   glFlush();
   glutSwapBuffers();
@@ -33,12 +68,13 @@ void myDisplay( void )  {
 }
 
 void tickBodyPart(BodyPart *part){
-    part->tick(5e-4);
 
+    part->tick(5e-4);
     Vec2 line[2];
     part->getLine(line);
     glBegin(GL_LINES);
 
+//    printf("%.2f %.2f  %.2f  %.2f \n", line[0]._x, line[0]._y, line[1]._x, line[1]._y);
     glVertex3f(line[0]._x, line[0]._y, .0);
     glVertex3f(line[1]._x, line[1]._y, .0);
 
