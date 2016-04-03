749d73fd2733e2d8d64ed5d7eb2d409cea360919
giangm9 <giangm9@gmail.com>
2016-04-03 14:18:41 +0700

    lab6 final

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -8,21 +8,21 @@
 
 
 enum TransformMode{
-	TM_TRANS = 0,
-	TM_ROTATE,
-	TM_SCALE
+    TM_TRANS = 0,
+    TM_ROTATE,
+    TM_SCALE
 };
 
 
 int len = 16;
 float house[] = {.0, .5, .5, 1.,
-								 .7, .8, .7, .9,
-								 .8, .9, .8, .9,
-								 .8, .7, 1., .5,
-								 .8, .5, .8, .0,
-								 .6, .0, .6, .2,
-								 .4, .2, .4, .0,
-								 .2, .0, .2, .5};
+		 .7, .8, .7, .9,
+		 .8, .9, .8, .9,
+		 .8, .7, 1., .5,
+		 .8, .5, .8, .0,
+		 .6, .0, .6, .2,
+		 .4, .2, .4, .0,
+		 .2, .0, .2, .5};
 
 TransformMode currentMode = TM_TRANS;
 
@@ -40,67 +40,67 @@ float gScaleY = 1.0f;
 
 
 void myInit( void )  {
-  glClearColor( 0.0, 0.0, 0.0, 0.0 );
-  glColor3f( 1.0, 1.0, 1.0 );
-  glPointSize( 3.0 );
-  glMatrixMode( GL_PROJECTION );
-  glLoadIdentity( );
-  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glClearColor( 0.0, 0.0, 0.0, 0.0 );
+    glColor3f( 1.0, 1.0, 1.0 );
+    glPointSize( 3.0 );
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity( );
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 
 }
 
 void displayText( float x, float y,  const char *string,
-									int r  = 255, int g = 255, int b = 255) {
-	int j = strlen( string );
+		  int r  = 255, int g = 255, int b = 255) {
+    int j = strlen( string );
 
-	glColor3f( r, g, b );
-	glRasterPos2f( x, y );
-	for( int i = 0; i < j; i++ ) {
-		glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18, string[i] );
-	}
+    glColor3f( r, g, b );
+    glRasterPos2f( x, y );
+    for( int i = 0; i < j; i++ ) {
+	glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18, string[i] );
+    }
 }
 
 
 
 void  drawHouse(){
-	glBegin(GL_LINE_LOOP);
-	for (int i = 0; i < len; i++){
-		glVertex2f(house[i<<1] - .5, house[(i<<1) + 1] - .5);
-	}
-	glEnd();
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < len; i++){
+	glVertex2f(house[i<<1] - .5, house[(i<<1) + 1] - .5);
+    }
+    glEnd();
 }
 
 
 void myDisplay( void )  {
-  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-  glColor3f(1.0f, 1.0f, 1.0f);
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glColor3f(1.0f, 1.0f, 1.0f);
 
-	glPushMatrix();
-	glTranslatef(gTx, gTy, .0f);
-	glRotatef(gRotate, 0.0, 0.0, 1.0);
-	glScalef(gScaleX, gScaleY, 1.0);
-	drawHouse();
-	glPopMatrix();
+    glPushMatrix();
+    glTranslatef(gTx, gTy, .0f);
+    glRotatef(gRotate, 0.0, 0.0, 1.0);
+    glScalef(gScaleX, gScaleY, 1.0);
+    drawHouse();
+    glPopMatrix();
 
-// show help text
-	displayText(.0,.07, "Press Q to change mode");
-	switch (currentMode){
-	case TM_TRANS:{
-		displayText(.0,.01, "Current Mode : translate (w/s/a/d to move)" );
-		break;
-	}
-	case TM_ROTATE:{
-		displayText(.0,.01, "Current Mode : rotate (a/d to rotate)" );
-		break;
-	}
-	case TM_SCALE:{
-		displayText(.0,.01, "Current Mode : scale (w/s/a/d to scale)" );
-	}
-	default: break;
-	}
+    // show help text
+    displayText(.0,.07, "Press Q to change mode");
+    switch (currentMode){
+    case TM_TRANS:{
+	displayText(.0,.01, "Current Mode : translate (w/s/a/d to move)" );
+	break;
+    }
+    case TM_ROTATE:{
+	displayText(.0,.01, "Current Mode : rotate (a/d to rotate)" );
+	break;
+    }
+    case TM_SCALE:{
+	displayText(.0,.01, "Current Mode : scale (w/s/a/d to scale)" );
+    }
+    default: break;
+    }
 
-  glFlush();
-  glutSwapBuffers();
+    glFlush();
+    glutSwapBuffers();
 
 
 }
@@ -108,54 +108,54 @@ void myDisplay( void )  {
 
 
 void keyboard (unsigned char key, int x, int y){
-	if (key == 'q'){
-		currentMode = (TransformMode)((currentMode + 1) % 3);
+    if (key == 'q'){
+	currentMode = (TransformMode)((currentMode + 1) % 3);
+    }
+
+
+    if (currentMode == TM_TRANS){
+	switch(key){
+	case 'w' : gTy += kDELTA_T; break;
+	case 's' : gTy -= kDELTA_T; break;
+	case 'a' : gTx -= kDELTA_T; break;
+	case 'd' : gTx += kDELTA_T; break;
 	}
 
+    }
 
-	if (currentMode == TM_TRANS){
-		switch(key){
-		case 'w' : gTy += kDELTA_T; break;
-		case 's' : gTy -= kDELTA_T; break;
-		case 'a' : gTx -= kDELTA_T; break;
-		case 'd' : gTx += kDELTA_T; break;
-		}
-
+    if (currentMode == TM_ROTATE){
+	switch(key){
+	case 'a' : gRotate += kDELTA_R; break;
+	case 'd' : gRotate -= kDELTA_R; break;
 	}
+    }
 
-	if (currentMode == TM_ROTATE){
-		switch(key){
-		case 'a' : gRotate += kDELTA_R; break;
-		case 'd' : gRotate -= kDELTA_R; break;
-		}
+    if (currentMode == TM_SCALE){
+	switch(key){
+	case 'w' : gScaleY += kDELTA_S; break;
+	case 's' : gScaleY -= kDELTA_S; break;
+	case 'a' : gScaleX -= kDELTA_S; break;
+	case 'd' : gScaleX += kDELTA_S; break;
 	}
-
-	if (currentMode == TM_SCALE){
-		switch(key){
-		case 'w' : gScaleY += kDELTA_S; break;
-		case 's' : gScaleY -= kDELTA_S; break;
-		case 'a' : gScaleX -= kDELTA_S; break;
-		case 'd' : gScaleX += kDELTA_S; break;
-		}
-	}
-	glutPostRedisplay();
+    }
+    glutPostRedisplay();
 
 }
 
 int main( int argc, char *argv[] )  {  
-  glutInit( &argc, argv );  
-  glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );  
-  glutInitWindowSize( 640, 480 );  
-  glutInitWindowPosition( 100, 150 );  
-	glutCreateWindow( "lab5" );
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
+    glutInitWindowSize( 640, 480 );
+    glutInitWindowPosition( 100, 150 );
+    glutCreateWindow( "lab5" );
 
 
-	myInit( );
-  glutDisplayFunc( myDisplay );
-	glutKeyboardFunc(keyboard);
+    myInit( );
+    glutDisplayFunc( myDisplay );
+    glutKeyboardFunc(keyboard);
 
 
-  glutMainLoop( );
+    glutMainLoop( );
 }
 
 
--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,73 +1,111 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
 #include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "Stick.h"
 
-#include "Parts.h"
 
-const int count = 5;
-Part* body[count];
+float ey = 5;
 
-void myInit( void )  {
+float rotate = .0;
 
-    // gl init
-    glClearColor( 0.0, 0.0, 0.0, 0.0 );
-    glColor3f( 1.0, 1.0, 1.0 );
-    glPointSize( 3.0 );
-    glMatrixMode( GL_PROJECTION );
-    glLoadIdentity( );
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 
-    // parts init
 
-    body[0] = new BodyPart();
-    body[1] = new ArmPart(body[0]);
-    body[2] = new HandPart(body[1]);
-    body[3] = new ArmPart(body[0], .9);
-    body[4] = new HandPart(body[3], .9);
-    body[5] = new UpLeg(body[0]);
-    body[6] = new LowLeg(body[5]);
-    body[7] = new UpLeg(body[0], .9);
-    body[8] = new LowLeg(body[7], .9);
+void init(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel (GL_FLAT);    
+    glViewport (0, 0, 500, 500);
+
+
+    gluLookAt(5, 5, 5, 2, 2, 2, 0, 1, 0);
 
 }
 
-void drawPart(Part* part){
+
+void dline(float x, float y, float z,
+           float r, float g, float b){
+    glColor3f(r, g, b);
+    glLineWidth(2);
     glBegin(GL_LINES);
-    glVertex3f(part->head.x, part->head.y, .0);
-    glVertex3f(part->getTail().x, part->getTail().y, .0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(x, y, z);
     glEnd();
+    glLineWidth(1);
 }
 
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-void stickmanDisplay( void )  {
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    glColor3f(1.0f, 1.0f, 1.0f);
+    glPushMatrix();
+    gluLookAt(5, ey, 5, 2, 2, 2, 0, 1, 0);
+    glRotatef(rotate, 0, 1, 0);
+
+    dline(1,0, 0, 1, 0, 0);
+    dline(0, 1,0, 0, 1, 0);
+    dline(0, 0, 1, 0, 0, 1);
 
 
-    for (int i = 0; i < 9; i++){
-        body[i]->tick(6e-4);
-        drawPart(body[i]);
-    }
+    glTranslatef(2, 1, 2);
+    glColor3f (1.0, 0, 0);
+    glutWireSphere(1.0, 20, 16);
 
-    glFlush();
+    glColor3f (.1, .6, .8);
+    glutWireCube(3.0f);
+
+    glPopMatrix();
     glutSwapBuffers();
+}
+
+void reshape (int w, int h)
+{
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    gluPerspective(80.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+
+    glMatrixMode(GL_MODELVIEW);
+
+    glLoadIdentity();    
+}
+
+void keyboard (unsigned char key, int x, int y)
+{    
+
+
+    switch (key) {
+    case 'w':
+        ey += .8;
+        break;
+    case 's':
+        ey -= .8;
+        break;
+    case 'a':
+        rotate += 10;
+        break;
+    case 'd':        
+        rotate -= 10;
+        break;
+    }
     glutPostRedisplay();
 }
 
+int main(int argc, char** argv)
+{
 
+    glutInit(&argc, argv);
+    Stick::ShowStickMan();
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (300, 100);
+    glutCreateWindow ("lab6 (w/s/a/d to move the camera)");
+    init ();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
 
-int main( int argc, char *argv[] )  {  
-    glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
-    glutInitWindowSize( 640, 480 );
-    glutInitWindowPosition( 100, 150 );
-    glutCreateWindow( "lab1" );
-
-
-    myInit( );
-    glutDisplayFunc( stickmanDisplay );
-
-    glutMainLoop( );
+    glutMainLoop();
+    return 0;
 }
-
-
